0457c6590dbf3529ce764d7bdbeb4f58fddca9e2
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2024-05-02 13:41:31 -0700

    code to access tokens

---
+++ src/gypsum/_utils.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+from typing import Optional
+
+
+def _rest_url(url: Optional[str] = None):
+    current = "https://gypsum.artifactdb.com"
+
+    if url is None:
+        return current
+    else:
+        return url
+
+
+def _cache_directory(dir: Optional[str] = None):
+    current = None
+    if current is None:
+        _from_env = os.environ.get("GYPSUM_CACHE_DIR", None)
+        if _from_env is not None:
+            if not os.path.exists(_from_env):
+                raise FileNotFoundError(
+                    f"Path {_from_env} does not exist or is not accessible."
+                )
+
+            current = _from_env
+        else:
+            current = os.path.join(str(Path.home()), "gypsum", "cache")
+
+    if dir is None:
+        return current
+    else:
+        if not os.path.exists(current):
+            raise FileNotFoundError(
+                f"Path {current} does not exist or is not accessible."
+            )
+
+        return current
+
+
+def _remove_slash_url(url: str):
+    if url.endswith("/"):
+        url = url.rstrip("/")
+
+    return url
---
+++ src/gypsum/auth.py
@@ -0,0 +1,180 @@
+import os
+import time
+from typing import Optional
+
+from filelock import FileLock
+from oauthlib.oauth2 import BackendApplicationClient
+from requests import get
+from requests_oauthlib import OAuth2Session
+
+from ._utils import _cache_directory, _remove_slash_url, _rest_url
+
+TOKEN_CACHE = {}
+
+
+def _token_cache_path(cache_dir):
+    return os.path.join(cache_dir, "credentials", "token.txt")
+
+
+def access_token(
+    full: bool = False, request: bool = True, cache_dir: Optional[str] = None
+):
+    """Get GitHub access tokens for authentication to the gypsum API's.
+
+    Args:
+        full:
+            Whether to return the full token details.
+            Defaults to False.
+
+        request:
+            Whether to request a new token if no token is found or the
+            current token is expired. Defaults to True.
+
+        cache_dir:
+            Path to the cache directory to store tokens.
+            Defaults to None, indicating token is not cached to disk.
+
+    Returns:
+        The GitHub token to access gypsum's resources.
+    """
+    global TOKEN_CACHE
+    expiry_leeway = 10  # number of seconds until use of the token.
+
+    def _token_func(x):
+        return x["token"] if not full else x
+
+    cache_dir = _cache_directory(cache_dir)
+
+    in_memory = TOKEN_CACHE.get("auth_info")
+    if in_memory is not None:
+        if in_memory["expires"] > time.time() + expiry_leeway:
+            return _token_func(in_memory)
+        else:
+            TOKEN_CACHE["auth_info"] = None
+
+    if cache_dir:
+        cache_path = _token_cache_path(cache_dir)
+        if os.path.exists(cache_path):
+            _lock = FileLock(cache_path)
+            with _lock:
+                with open(cache_path, "r") as file:
+                    dump = file.read().splitlines()
+
+            exp = float(dump[2])
+            if exp > time.time() + expiry_leeway:
+                info = {"token": dump[0], "name": dump[1], "expires": exp}
+                TOKEN_CACHE["auth_info"] = info
+                return _token_func(info)
+            else:
+                os.unlink(cache_path)
+
+    if request:
+        payload = set_access_token(cache=cache_dir)
+        return _token_func(payload) if payload else None
+    else:
+        return None
+
+
+def set_access_token(
+    token: Optional[str] = None,
+    app_url: Optional[str] = _rest_url(),
+    app_key: Optional[str] = None,
+    app_secret: Optional[str] = None,
+    github_url: str = "https://api.github.com",
+    user_agent: Optional[str] = None,
+    cache_dir: Optional[str] = None,
+):
+    """Set GitHub access tokens for authentication to the gypsum API's.
+
+    Args:
+        token:
+            A String containing Github's personal access token.
+            Defaults to None.
+
+        app_url:
+            URL to the gypsum REST API.
+
+        app_key:
+            Key to the GitHub oauth app.
+
+        app_secret:
+            Secret to the GitHub oauth app.
+
+        github_url:
+            URL to GitHub's API.
+
+        user_agent:
+            Specify the user agent for queries to various endpoints.
+
+        cache_dir:
+            Path to the cache directory to store tokens.
+            Defaults to None, indicating token is not cached to disk.
+
+    Returns:
+        The GitHub token to access gypsum's resources.
+    """
+    global TOKEN_CACHE
+
+    cache_dir = _cache_directory(cache_dir)
+
+    if not token:
+        if not app_key and not app_secret:
+            raise ValueError(
+                "'app_key' and 'app_secret' must be provided if 'token' is not specified."
+            )
+
+        if not app_key or not app_secret:
+
+            _url = f"{_remove_slash_url(app_url)}/credentials/github-app"
+            headers = {}
+            if user_agent:
+                headers["User-Agent"] = user_agent
+
+            r = get(_url, headers=headers)
+            r.raise_for_status()
+
+            info = r.json()
+            app_key < -info["id"]
+            app_secret < -info["secret"]
+
+        client = BackendApplicationClient(client_id=app_key)
+        oauth = OAuth2Session(client=client)
+        ores = oauth.fetch_token(
+            token_url="https://github.com/login/oauth/access_token",
+            client_id=app_key,
+            client_secret=app_secret,
+        )
+
+        token = ores["access_token"]
+
+    headers = {}
+    if user_agent:
+        headers["User-Agent"] = user_agent
+
+    headers["Authorizaton"] = f"Bearer {token}"
+
+    req = get(f"{_remove_slash_url(github_url)}/user", headers=headers)
+    req.raise_for_status()
+
+    res = req.json()
+
+    name = res["login"]
+    expires_header = req.headers.get("github-authentication-token-expiration")
+    expiry = float(expires_header.split()[0]) if expires_header else float("inf")
+
+    if not cache_dir:
+        cache_path = _token_cache_path(cache_dir)
+        os.makedirs(cache_path, exist_ok=True)
+
+        _lock = FileLock(cache_path)
+        with _lock:
+            with open(cache_path, "w") as file:
+                file.write("\n".join([token, name, str(expiry)]))
+
+        os.chmod(
+            cache_path, 0o600
+        )  # prevent anyone else from reading this on shared file systems.
+
+    vals = {"token": token, "name": name, "expires": expiry}
+    TOKEN_CACHE["auth_info"] = vals
+    return vals
