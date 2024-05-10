8eb6d4d18ec4ae2c1e6e6193438af57d2c95e87f
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2024-05-10 19:02:52 -0400

    Implement fetch operations (#4)

--- src/gypsum_client/_utils.py
+++ src/gypsum_client/_utils.py
@@ -1,8 +1,14 @@
+import json
 import os
+import shutil
+import tempfile
+from datetime import datetime, timezone
 from pathlib import Path
 from typing import Optional
+from urllib.parse import quote_plus
 
 import requests
+from filelock import FileLock
 
 __author__ = "Jayaram Kancherla"
 __copyright__ = "Jayaram Kancherla"
@@ -35,12 +41,10 @@ def _cache_directory(dir: Optional[str] = None):
     if dir is None:
         return current
     else:
-        if not os.path.exists(current):
-            raise FileNotFoundError(
-                f"Path {current} does not exist or is not accessible."
-            )
+        if not os.path.exists(dir):
+            raise FileNotFoundError(f"Path {dir} does not exist or is not accessible.")
 
-        return current
+        return dir
 
 
 def _remove_slash_url(url: str):
@@ -77,3 +81,105 @@ def _list_for_prefix(
         resp = [_remove_slash_url(val) for val in resp if not val.startswith("..")]
 
     return resp
+
+
+def _fetch_json(path: str, url: str):
+    full_url = f"{url}/file/{quote_plus(path)}"
+
+    req = requests.get(full_url)
+    req.raise_for_status()
+
+    return req.json()
+
+
+BUCKET_CACHE_NAME = "bucket"
+
+
+def _fetch_cacheable_json(
+    project: str,
+    asset: str,
+    version: str,
+    path: str,
+    cache: str,
+    url: str,
+    overwrite: bool,
+):
+    bucket_path = f"{project}/{asset}/{version}/{path}"
+
+    if cache is None:
+        return _fetch_json(bucket_path, url=url)
+    else:
+        _out_path = os.path.join(
+            cache, BUCKET_CACHE_NAME, project, asset, version, path
+        )
+
+        _save_file(bucket_path, destination=_out_path, overwrite=overwrite, url=url)
+
+        with open(_out_path, "r") as jf:
+            return json.load(jf)
+
+
+def _save_file(
+    path: str, destination: str, overwrite: bool, url: str, error: bool = True
+):
+    if overwrite is True or not os.path.exists(destination):
+        os.makedirs(os.path.dirname(destination), exist_ok=True)
+
+        _lock = FileLock(destination)
+        with _lock:
+            with tempfile.NamedTemporaryFile(
+                dir=os.path.dirname(destination), delete=False
+            ) as tmp_file:
+                try:
+                    full_url = f"{url}/file/{quote_plus(path)}"
+
+                    req = requests.get(full_url, stream=True)
+                    req.raise_for_status()
+
+                    for chunk in req.iter_content(chunk_size=None):
+                        tmp_file.write(chunk)
+                except Exception as e:
+                    if error:
+                        raise Exception(f"Failed to save '{path}'; {str(e)}.")
+                    else:
+                        return False
+
+                # Rename the temporary file to the destination
+                shutil.move(tmp_file.name, destination)
+
+    return True
+
+
+def _cast_datetime(x):
+    if x.endswith("Z"):
+        x = x[:-1]
+
+    # Remove fractional seconds.
+    x = x.split(".")[0]
+
+    return datetime.strptime(x, "%Y-%m-%dT%H:%M:%S").astimezone(tz=timezone.utc)
+
+
+def _rename_file(src: str, dest: str):
+    try:
+        os.rename(src, dest)
+    except OSError:
+        try:
+            # If renaming fails, try copying
+            shutil.copy(src, dest)
+            os.remove(src)  # Remove the original file after copying
+        except Exception as e:
+            raise RuntimeError(
+                f"Cannot move temporary file for '{src}' to its destination '{dest}': {e}."
+            )
+
+
+def _download_and_rename_file(url: str, dest: str):
+    tmp = tempfile.NamedTemporaryFile(dir=os.path.dirname(dest), delete=False).name
+    req = requests.get(url, stream=True)
+
+    with open(tmp, "wb") as f:
+        for chunk in req.iter_content():
+            f.write(chunk)
+
+    _rename_file(tmp, dest)
---
+++ src/gypsum_client/fetch_metadata_database.py
@@ -0,0 +1,112 @@
+import os
+import tempfile
+import time
+
+import requests
+from filelock import FileLock
+
+from ._utils import _cache_directory, _download_and_rename_file
+
+__author__ = "Jayaram Kancherla"
+__copyright__ = "Jayaram Kancherla"
+__license__ = "MIT"
+
+LAST_CHECK = {"req_time": None, "mod_time": None}
+
+
+def fetch_metadata_database(
+    name: str = "bioconductor.sqlite3", cache_dir: str = None, overwrite: bool = False
+) -> str:
+    """Fetch the SQLite database containing metadata from the gypsum backend.
+
+    This function will automatically check for updates to the SQLite files
+    and will download new versions accordingly. New checks are performed when one hour
+    or more has elapsed since the last check. If the check fails, a warning is raised
+    and the function returns the currently cached file.
+
+    Args:
+        name:
+            Name of the database.
+            This can be the name of any SQLite file in
+            https://github.com/ArtifactDB/bioconductor-metadata-index/releases/tag/latest.
+
+            Defaults to "bioconductor.sqlite3".
+
+        cache_dir:
+            Path to the cache directory.
+
+            Defaults to None.
+
+        overwrite:
+            Whether to overwrite existing file.
+
+            Defaults to False.
+
+    Returns:
+        Path to the downloaded database.
+    """
+    base_url = "https://github.com/ArtifactDB/bioconductor-metadata-index/releases/download/latest/"
+
+    if cache_dir is None:
+        cache_path = tempfile.NamedTemporaryFile(suffix=".sqlite3").name
+    else:
+        cache_dir = os.path.join(_cache_directory(cache_dir), "databases")
+
+        cache_path = os.path.join(cache_dir, name)
+        if not os.path.exists(cache_path):
+            os.makedirs(os.path.dirname(cache_path), exist_ok=True)
+
+        if os.path.exists(cache_path) and not overwrite:
+            old_lastmod_raw = None
+
+            _lock = FileLock(cache_path)
+            if not _lock.is_locked:
+                old_lastmod_raw = open(cache_path + ".modified").readlines()
+
+            old_lastmod = float(old_lastmod_raw[0])
+            new_lastmod = get_last_modified_date(base_url)
+
+            print(old_lastmod, new_lastmod)
+
+            if new_lastmod is not None and old_lastmod == new_lastmod:
+                return cache_path
+
+    print("why is it here")
+    print(cache_path)
+    _lock = FileLock(cache_path)
+    with _lock:
+        mod_path = cache_path + ".modified"
+        _download_and_rename_file(base_url + "modified", mod_path)
+        _download_and_rename_file(base_url + name, cache_path)
+
+    LAST_CHECK["req_time"] = get_current_unix_time()
+    LAST_CHECK["mod_time"] = float(open(mod_path).readline())
+
+    return cache_path
+
+
+def get_current_unix_time():
+    return time.time() * 1000  # milliseconds
+
+
+def get_last_modified_date(base_url):
+    curtime = get_current_unix_time()
+    if (
+        LAST_CHECK["req_time"] is not None
+        and LAST_CHECK["req_time"] + 60 * 60 * 1000 >= curtime
+    ):
+        return LAST_CHECK["mod_time"]
+
+    mod_time = None
+    try:
+        url = base_url + "modified"
+        response = requests.get(url)
+        mod_time = float(response.text)
+    except Exception as e:
+        print("Failed to check the last modified timestamp:", str(e))
+
+    if mod_time is not None:
+        LAST_CHECK["req_time"] = curtime
+        LAST_CHECK["mod_time"] = mod_time
+
+    return mod_time
